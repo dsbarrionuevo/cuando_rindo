a6f06020f7ca0e4658583290f9ac166f5c0fa8da
Facu <facumallia@hotmail.com>
2015-09-04 16:10:41 -0300

    adapter e interfaz

--- app/src/main/java/com/labsis/cuandorindo/ActivityPrincipal.java
+++ app/src/main/java/com/labsis/cuandorindo/ActivityPrincipal.java
@@ -2,16 +2,20 @@ package com.labsis.cuandorindo;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
 
-public class Principal extends ActionBarActivity {
+public class ActivityPrincipal extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_principal);
+
+
+
     }
 
     @Override
