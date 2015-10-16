8cbcee9e689b5eebd384d1ae62be548c0adcf4ff
fedea <fede.amura@gmail.com>
2015-10-16 16:52:16 -0300

    busqueda

--- app/src/main/java/com/labsis/cuandorindo/ActivityPrincipal.java
+++ app/src/main/java/com/labsis/cuandorindo/ActivityPrincipal.java
@@ -8,14 +8,20 @@ import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.afollestad.materialdialogs.MaterialDialog;
 import com.labsis.cuandorindo.Adaptadores.AdaptadorExamenes;
 import com.labsis.cuandorindo.DAO.ExamenDAO;
+import com.labsis.cuandorindo.DAO.TipoExamenDAO;
 import com.labsis.cuandorindo.Entidades.Examen;
+import com.labsis.cuandorindo.Entidades.TipoExamen;
 
 public class ActivityPrincipal extends AppCompatActivity {
 
@@ -23,6 +29,9 @@ public class ActivityPrincipal extends AppCompatActivity {
     private RecyclerView lstExamenes;
     private AdaptadorExamenes adaptadorExamenes;
     private Toolbar toolbar;
+
+    private TextView textoTitulo;
+    private EditText txtBuscar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -50,7 +59,7 @@ public class ActivityPrincipal extends AppCompatActivity {
                             public void onPositive(MaterialDialog dialog) {
                                 super.onPositive(dialog);
                                 if (ExamenDAO.getInstance().borrar(examen.getId()) == 1) {
-                                    adaptadorExamenes.quitar(pos);
+                                    adaptadorExamenes.quitar(examen);
                                 } else {
                                     Toast.makeText(ActivityPrincipal.this, "Error al borrar el examen", Toast.LENGTH_LONG).show();
                                 }
@@ -86,11 +95,39 @@ public class ActivityPrincipal extends AppCompatActivity {
             public boolean onMenuItemClick(MenuItem item) {
                 switch (item.getItemId()) {
                     case R.id.toolbarPrincipal_Buscar:
-                        Toast.makeText(ActivityPrincipal.this, "Buscar", Toast.LENGTH_LONG).show();
+                        if (txtBuscar.getVisibility() == View.VISIBLE) {
+                            txtBuscar.setVisibility(View.GONE);
+                            textoTitulo.setVisibility(View.VISIBLE);
+                        } else {
+                            txtBuscar.setVisibility(View.VISIBLE);
+                            textoTitulo.setVisibility(View.GONE);
+                        }
                         break;
                 }
                 return false;
             }
         });
+
+        textoTitulo = (TextView) toolbar.findViewById(R.id.textoTitulo);
+        txtBuscar = (EditText) toolbar.findViewById(R.id.txtBusqueda);
+
+        txtBuscar.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                String texto = txtBuscar.getText().toString();
+                adaptadorExamenes.setFIltro(texto);
+            }
+        });
+
     }
 }
